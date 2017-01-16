ffb5aef912863f3f0eecd3364491cbaedd3223ab
dengdetian0603 <dengdetian0603@gmail.com>
2017-01-15 23:12:39 -0500

    make plots 0116

--- Code/SensitivityAnalysis.R
+++ Code/SensitivityAnalysis.R
@@ -1,11 +1,13 @@
-setwd("~/Documents/JHSPH/Research/S.Zeger/PQ_Models/WorkSpace/SensitivityAnalysis")
+setwd("~/Documents/JHSPH/Research/S.Zeger/PQ_Models/WorkSpace/AD.SensitivityAnalysis")
+setwd("~/Documents/JHSPH/Research/S.Zeger/PQ_Models/WorkSpace/MultinomCaseAnalysis")
+
 suppressMessages(source("../../Code/PerchDataSimulation.R"))
 suppressMessages(source("../../Code/PostProcessJAGS.R"))
 suppressMessages(source("../../Code/PerchPlots.R"))
 
 library(ICC)
 
-file.names = system("ls ./SC1-2*.csv", intern = TRUE)
+file.names = system("ls ./*.csv", intern = TRUE)
 # -----------------------------------------------------------------------------
 f = 14
 file = file.names[f]
@@ -47,7 +49,7 @@ plot.obj = PlotByCombination(cell.prob.fit0, sim.obj,
                              contrast = "baker", baker.result = baker.fit)
 do.call(grid.arrange, plot.obj)
 # -----------------------------------------------------------------------------
-err.tab = read.csv("../SC1_Reg_SensAnalysis.csv")
+err.tab = read.csv("../AccurateData_Reg_SensAnalysis.csv")
 pr.nonindep.mean = with(err.tab, pind.a/(pind.a + pind.b))
 pr.nonindep.sd = round(with(err.tab,
                             sqrt(pind.a * pind.b / (pind.a + pind.b) ^ 2
@@ -55,7 +57,7 @@ pr.nonindep.sd = round(with(err.tab,
 err.tab = cbind(err.tab, pr.nonindep.mean, pr.nonindep.sd)[, -c(1, 4:11)]
 err.tab.sc1 = err.tab[!is.na(err.tab$theta1.mu), ]
 err.tab.baker =  err.tab[is.na(err.tab$theta1.mu), ]
-err.tab.baker$spec = c("Singletons+Pairs", "Singletons")
+err.tab.baker$spec = c("Singletons", "Singletons+Pairs")
 
 
 g = ggplot()
@@ -84,36 +86,38 @@ for (f in 1:length(file.names)) {
   pooled.sim.fit  = rbind(pooled.sim.fit, sim.fit)
 }
 prior.cat.0 = pooled.sim.fit$prior.cat 
+pooled.sim.fit0 = pooled.sim.fit
 
 # pooled.sim.fit$prior.cat = prior.cat.0
 # o111 = order(pooled.sim.fit[pooled.sim.fit$tid == 50, "Mu.1.1."])
 # prior.cat.new = o111[pooled.sim.fit$prior.cat]
 # pooled.sim.fit$prior.cat = prior.cat.new
 
+pooled.sim.fit = pooled.sim.fit0[pooled.sim.fit0$tid < 20, ]
 gp = list()
 gp[[1]] = ggplot(data = pooled.sim.fit,
-                 aes(x = prior.cat, y = Mu.2.1., group = tid)) +
+                 aes(x = prior.cat, y = Mu.1.1., group = tid)) +
           geom_jitter(alpha = 0.5, width = 0.1) + 
           geom_line(aes(col = tid), alpha = 0.3)
 gp[[2]] = ggplot(data = pooled.sim.fit,
-                 aes(x = prior.cat, y = Mu.2.2., group = tid)) +
+                 aes(x = prior.cat, y = Mu.1.2., group = tid)) +
   geom_jitter(alpha = 0.5, width = 0.1) + 
   geom_line(aes(col = tid), alpha = 0.3)
 gp[[3]] = ggplot(data = pooled.sim.fit,
-                 aes(x = prior.cat, y = Mu.2.3., group = tid)) +
+                 aes(x = prior.cat, y = Mu.1.3., group = tid)) +
   geom_jitter(alpha = 0.5, width = 0.1) + 
   geom_line(aes(col = tid), alpha = 0.3)
 gp[[4]] = ggplot(data = pooled.sim.fit,
-                 aes(x = prior.cat, y = Mu.2.4., group = tid)) +
+                 aes(x = prior.cat, y = Mu.1.4., group = tid)) +
   geom_jitter(alpha = 0.5, width = 0.1) + 
   geom_line(aes(col = tid), alpha = 0.3)
 gp[[5]] = ggplot(data = pooled.sim.fit,
-                 aes(x = prior.cat, y = Mu.2.5., group = tid)) +
+                 aes(x = prior.cat, y = Mu.1.5., group = tid)) +
   geom_jitter(alpha = 0.5, width = 0.1) + 
   geom_line(aes(col = tid), alpha = 0.3)
 
 do.call(grid.arrange, gp)
-# ICCest(x = tid, y = Mu.2.1., data = pooled.sim.fit)$ICC
+# ICCest(x = tid, y = Mu.2.1., data = pooled.sim.fit0)$ICC
 
 # ---------------------------------------------------------------------------
 d1 = pooled.sim.fit[pooled.sim.fit$prior.cat == 2, c("tid", "Mu.1.1.")]
